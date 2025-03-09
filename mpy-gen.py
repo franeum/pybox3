2dd50bee39417f204327246e61a16d4f2481f9c2
franeum <elettrochuck@gmail.com>
2025-03-09 16:23:42 +0100

    update

--- mpy-gen.py
+++ mpy-gen.py
@@ -4,14 +4,21 @@ import sys
 import mpy_cross as mpy
 from pathlib import Path
 import shutil
+import time
 
-input, output = Path(".") / Path(sys.argv[1]), Path(".") / Path(sys.argv[2])
-input_files = input.glob("*.py")
+_input, output = Path(".") / Path(sys.argv[1]), Path(".") / Path(sys.argv[2])
 
-
-for file in input_files:
+print("GENERATING MPY...")
+for file in _input.glob("*.py"):
     mpy.run(f"{file}")
-    shutil.copyfile(file, f"./{output}/{file.stem}.mpy")
 
-for file in input.glob("*.mpy"):
+time.sleep(1)
+print("\t...done")
+
+print("MOVE TO DESTINATION")
+
+for file in _input.glob("*.mpy"):
+    shutil.copy(file, output)
     Path(file).unlink()
+
+print("\t...done")
